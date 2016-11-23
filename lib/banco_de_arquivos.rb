daa8e2f98de47464d7ee82e77250d7d1e0f22e5d
renan-garcia <email@renangarcia.me>
2016-11-22 22:53:04 -0200

    Criado banco de arquivos

---
+++ lib/banco_de_arquivos.rb
@@ -0,0 +1,10 @@
+require 'yaml'
+
+class BancoDeArquivos
+  def salva(livro)
+    File.open("livros.yml", "a") do |arquivo|
+      arquivo.puts YAML.dump(livro)
+      arquivo.puts ""
+    end
+  end
+end
